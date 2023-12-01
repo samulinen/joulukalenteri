658a934e125db1c246e6c8a91e830af4cc6e6d7c
samulinen <147338819+samulinen@users.noreply.github.com>
2023-12-01 15:00:28 +0200

    Add files via upload

---
+++ joulukalenteri.js
@@ -0,0 +1,72 @@
+function Function1() {
+		alert("Sydän ihan pakahtuu, tuoksuu piparit ja joulupuu.")
+}
+function Function2() {
+		alert("Joulun taika, rauhan aika!")
+}
+function Function3() {
+	alert("Hiljaisten hankien sinistä taikaa, joulu on ihmeiden iloista aikaa.")
+}
+function Function4() {
+	alert("Piparit paistaen jokaisen suussa maistuen!")
+}
+function Function5() {
+	alert("Tule joulu, tule lämpö, tule muistojen ihana maa!")
+}
+function Function6() {
+	alert("Hyvää itsenäisyyspäivää!")
+}
+function Function7() {
+	alert("Kaunis on aamu valkoinen keskellä luonnon hiljaisen. Tonttu hiihtelee ihaillen loistoa helmihankien.")
+}
+function Function8() {
+	alert("Taivahalta sataa lunta, on kaunis joulun valtakunta.")
+}
+function Function9() {
+	alert("On Joulunaika leppoisaa, se hyvän mielen aikaan saa.")
+}
+function Function10() {
+	alert("Jollei joulu ole sydämessäsi, et voi koskaan löytää sitä kuusen alta.")
+}
+function Function11() {
+	alert("Toivon jouluusi rauhaa ja hiljaisuutta. Toivon tähtien valoisuutta.")
+}
+function Function12() {
+	alert("Ennen kesä lehmättä kuin joulu akatta")
+}
+function Function13() {
+	alert("Kellot jo kaikuvat kaamoksen yössä, yhä vain hyörimme kiireessä, työssä.")
+}
+function Function14() {
+	alert("Joulukuu on täynnä kiirettä ja juoksua, paperin rapinaa, piparin tuoksua.")
+}
+function Function15() {
+	alert("Hiljaa illan hämärässä joulu-ukko kulkee. Pimenevi metsän rinne yöhön seudun sulkee.")
+}
+function Function16() {
+	alert("Kuulkaa joulun sanomaa, josta kertovat tähdet ja rauhaisa maa…")
+}
+function Function17() {
+	alert("Monta täyttä kauhallista joulumieltä rauhallista!")
+}
+function Function18() {
+	alert("Tähtinen taivas, talvinen tie, jouluun meidät jälleen vie.")
+}
+function Function19() {
+	alert("Olkoon joulupyhäsi leppoisat, hetkesi hauskat, mieluisat!")
+}
+function Function20() {
+	alert("Joulun valoa ja lämpöä talven pakkasiin.")
+}
+function Function21() {
+	alert("On valkea lumi peittänyt maan. Jo käymme joulua juhlimaan.")
+}
+function Function22() {
+	alert("Oi ihanaa joulun aikaa, ilmassa väreilee tiettyä taikaa")
+}
+function Function23() {
+	alert("Joulu tulla jollottaa ja kaikki pukkia tollottaa")
+}
+function Function24() {
+	alert("Hyvää joulua!")
+}
